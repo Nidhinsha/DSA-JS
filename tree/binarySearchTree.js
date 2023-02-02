8326f2781cba5f548d330f31b7bc3e2b2015e703
Nidhinsha <nidhinsha64@gmail.com>
2023-02-02 21:13:30 +0530

    binary tree search insertion with comments

--- tree/binarySearchTree.js
+++ tree/binarySearchTree.js
@@ -20,11 +20,13 @@ class BinarySearchTree{
         const newNode = new Node(value)
 
         if (this.isEmpty()) {
+            
             this.root = newNode
+            console.log("adding value when tree is empty");
         } else {
-            console.log("++++++ calling the recursive fctn");
+           
             this.insertNode(this.root,newNode) // used for recurtion 
-
+            console.log("++++++ calling the recursive fctn");
         }
     }
 
@@ -33,16 +35,42 @@ class BinarySearchTree{
         // checking the left sub tree
         if(newNode.value < root.value){
             if (root.left === null) {
+                
                 root.left = newNode
+                console.log("when left is nulll");
             }else{
+                
                 this.insertNode(root.left,newNode)
+                console.log(root.left,newNode,"add value to left ");
             }
         }else{
              // checking the right sub tree
             if (root.right == null) {
+              
                 root.right = newNode
+                console.log("when right is nulll");
             }else{
+             
                 this.insertNode(root.right,newNode)
+                console.log(root.right,newNode,"add value to left ");
+            }
+        }
+    }
+
+    search(root,value){
+        console.log(root,value,"********search root and value");
+        if (!root) {
+            return false
+        } else {
+            if (root.value === value) {
+                console.log(root.value,value,"********element is in root");
+                return true
+            }else if(value < root.value){
+                console.log(value,root.value,"********the value < root");
+               return this.search(root.left,value)
+            }else{
+                console.log(value,root.value,"********the value > root");
+               return this.search(root.right,value)
             }
         }
     }
@@ -53,3 +81,7 @@ console.log("The tree is ",treeSearch.isEmpty());
 treeSearch.insert(10)
 treeSearch.insert(5)
 treeSearch.insert(15)
+
+console.log("$$$$$$$",treeSearch.search(treeSearch.root,10));
+console.log("$$$$$$$",treeSearch.search(treeSearch.root,5));
+console.log("$$$$$$$",treeSearch.search(treeSearch.root,15));
