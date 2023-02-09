e676f9786034286c1cd2c1cd86fd7a9561005605
Nidhinsha <nidhinsha64@gmail.com>
2023-02-09 21:46:18 +0530

    again nothing to commit

---
+++ heap/minHeap3.js
@@ -0,0 +1,28 @@
+class minHeap3{
+    constructor(){
+        this.heap = [null]
+    }
+    insert(num){
+        this.heap.push(num)
+        if (this.heap.length > 2) {
+            let index = this.heap.length - 1
+            while (this.heap[index]< this.heap[Math.floor(index/2)]) {
+                if (index >= 1) {
+                    [this.heap[Math.floor(index/2)],this.heap[index]] = [this.heap[index],this.heap[Math.floor(index/2)]]
+                }
+                if (Math.floor(index/2) > 1) {
+                    index = Math.floor(index/2)
+                } else {
+                    break
+                }
+            }
+        }
+    }
+}
+
+const heap = new minHeap3()
+heap.insert(9)
+heap.insert(9)
+heap.insert(6)
+heap.insert(70)
+console.log(heap.heap);
